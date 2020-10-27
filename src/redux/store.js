e13a2705f9333019b1c8afa13b6bcb24ca7a8e15
jianh-zhou <164377567@qq.com>
2020-10-27 16:53:13 +0800

    test

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,8 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-// import f from 'reac'
+import { composeWithDevTools } from 'redux-devtools-extension'
+let middleWarr=applyMiddleware(thunk)
+if (process.env.NODE_ENV === 'development') {
+  middleWarr=composeWithDevTools(middleWarr)
+}
+// export default 
